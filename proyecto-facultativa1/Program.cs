b35ce4be08832c042869a5137a5aa59871d9fc60
JosueProgrammer <bermudezjosue183@gmail.com>
2024-12-03 01:49:07 -0600

    program

--- proyecto-facultativa1/Program.cs
+++ proyecto-facultativa1/Program.cs
@@ -7,53 +7,37 @@ using proyecto_facultativa1.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Inyección de Entity Framework
+
+//Inyeccion de Entity Framework
 builder.Services.AddDbContext<ProductManagementContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Inyección de dependencias personalizadas
-builder.Services.AddScoped<ICrud<Productos>, ProductoRepository>();
+builder.Services.AddScoped <ICrud<Productos>, ProductoRepository>();
 builder.Services.AddScoped<ICrudServices<ProductoResponseDto, ProductoInsertDto, ProductoUpdateDto>, ProductoServices>();
+
 
 builder.Services.AddScoped<ICrud<Proveedores>, ProveedorRepository>();
 builder.Services.AddScoped<ICrudServices<ProveedoresResponseDto, ProveedoresInsertDto, ProveedoresUpdateDto>, ProveedoresServices>();
 
-// CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        builder => builder.AllowAnyOrigin()
-                           .AllowAnyMethod()
-                           .AllowAnyHeader());
-});
-
 var app = builder.Build();
 
-// Configuración del pipeline HTTP
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Manejo de excepciones
-app.UseExceptionHandler("/error");
-app.UseHsts();
-
 app.UseHttpsRedirection();
-app.UseCors("AllowAll");
-app.UseAuthorization();
-app.MapControllers();
 
-// Migraciones automáticas
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<ProductManagementContext>();
-    dbContext.Database.Migrate();
-}
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
