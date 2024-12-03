98d8ec61f96ef4117d544f27f576bb43cb2df229
JosueProgrammer <bermudezjosue183@gmail.com>
2024-12-03 01:43:37 -0600

    cambio en program

--- proyecto-facultativa1/Program.cs
+++ proyecto-facultativa1/Program.cs
@@ -7,37 +7,53 @@ using proyecto_facultativa1.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-//Inyeccion de Entity Framework
+// Inyección de Entity Framework
 builder.Services.AddDbContext<ProductManagementContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped <ICrud<Productos>, ProductoRepository>();
+// Inyección de dependencias personalizadas
+builder.Services.AddScoped<ICrud<Productos>, ProductoRepository>();
 builder.Services.AddScoped<ICrudServices<ProductoResponseDto, ProductoInsertDto, ProductoUpdateDto>, ProductoServices>();
-
 
 builder.Services.AddScoped<ICrud<Proveedores>, ProveedorRepository>();
 builder.Services.AddScoped<ICrudServices<ProveedoresResponseDto, ProveedoresInsertDto, ProveedoresUpdateDto>, ProveedoresServices>();
 
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Configuración del pipeline HTTP
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Manejo de excepciones
+app.UseExceptionHandler("/error");
+app.UseHsts();
+
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
 app.UseAuthorization();
-
 app.MapControllers();
+
+// Migraciones automáticas
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ProductManagementContext>();
+    dbContext.Database.Migrate();
+}
 
 app.Run();
