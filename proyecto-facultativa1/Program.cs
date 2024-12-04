8e51add35ce20d4780bc9fb1ba87769f0e9469fa
JosueProgrammer <bermudezjosue183@gmail.com>
2024-12-03 18:50:34 -0600

    config

--- proyecto-facultativa1/Program.cs
+++ proyecto-facultativa1/Program.cs
@@ -7,37 +7,42 @@ using proyecto_facultativa1.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-//Inyeccion de Entity Framework
+// Inyección de Entity Framework
 builder.Services.AddDbContext<ProductManagementContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped <ICrud<Productos>, ProductoRepository>();
+// Registrar servicios y repositorios
+builder.Services.AddScoped<ICrud<Productos>, ProductoRepository>();
 builder.Services.AddScoped<ICrudServices<ProductoResponseDto, ProductoInsertDto, ProductoUpdateDto>, ProductoServices>();
-
 
 builder.Services.AddScoped<ICrud<Proveedores>, ProveedorRepository>();
 builder.Services.AddScoped<ICrudServices<ProveedoresResponseDto, ProveedoresInsertDto, ProveedoresUpdateDto>, ProveedoresServices>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuración del pipeline HTTP
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Configuración para habilitar o deshabilitar redirección HTTPS
+if (!builder.Configuration.GetValue<bool>("DisableHttpsRedirection"))
+{
+    app.UseHttpsRedirection();
+}
+else
+{
+    Console.WriteLine("Redirección HTTPS está deshabilitada según la configuración.");
+}
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
